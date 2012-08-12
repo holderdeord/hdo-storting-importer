daeabdedf829e4d4c9b4a802c6ec18e763c32f32
Jari Bakken <jari.bakken@gmail.com>
2012-08-13 01:51:16 +0200

    Parse xlsx promises + fix schema for date.

--- lib/hdo/storting_importer/promise.rb
+++ lib/hdo/storting_importer/promise.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 
-require 'csv'
+require 'roo'
 
 module Hdo
   module StortingImporter
@@ -31,42 +31,70 @@ module Hdo
         Util.json_pretty example
       end
 
-      # doesn't really belong here - data source?
-      def self.from_fusion_table(table_id, api_key)
-        table = FusionTable.new(api_key)
+      def self.from_xlsx(path)
+        table  = Excelx.new(path)
+        raise "empty spreadsheet" unless table.first_row
 
-        column_names = table.columns_for(table_id).map { |e| e['name'] }.map { |e| e.inspect }.join(",")
-        row_count = Integer(table.query("select count(rowid) from #{table_id}", :rows => true).flatten.first)
+        errors = []
 
-        # do this in batches of 100
-        limit = 100
-        result = []
+        # 0 is empty, 1 is header
+        promises = 2.upto(table.last_row).map { |idx|
+          data = table.row(idx)
 
-        (0..row_count).step(limit) do |offset|
-          sql = %{SELECT #{column_names},"rowid" FROM #{table_id} OFFSET #{offset} LIMIT #{limit}}
+          Hdo::StortingImporter.logger.info "promise row: #{data.inspect}"
 
-          table.query(sql, :rows => true).map do |data|
-            Hdo::StortingImporter.logger.info "promise row: #{data.inspect}"
+          external_id = data.fetch(0).to_i.to_s
+          date        = data.fetch(1)
+          party       = data.fetch(2)
+          body        = data.fetch(3)
+          general     = data.fetch(4).to_s.strip.downcase
+          categories  = data.fetch(5)
+          source      = data.fetch(6)
+          page        = data.fetch(7)
 
-            party = data.fetch(0)
-            body = data.fetch(1)
-            general = data.fetch(2).downcase
-
-            unless ['ja', 'nei'].include?(general.downcase)
-              raise "unexpected value for general: #{general.inspect}"
-            end
-
-            categories  = data.fetch(3)
-            source      = data.fetch(4)
-            page        = data.fetch(5)
-            date        = data.fetch(6)
-            external_id = data.fetch(7)
-
-            result << new(external_id, party, body, general == 'ja', categories, source, page, date)
+          unless %w[ja nei].include?(general)
+            errors << "row #{external_id}: unexpected value #{general.inspect} for general"
+            next
           end
+
+          if page.to_s !~ /^\d+(\.\d+)?$/ || page.to_i == 0
+            errors << "row #{external_id}: unexpected value #{page.inspect} for page"
+            next
+          end
+
+          unless party
+            errors << "row #{external_id}: party missing"
+            next
+          end
+
+          unless body =~ /\.$/
+            body << "."
+          end
+
+          promise = new external_id,
+                        party,
+                        body,
+                        general.downcase == "ja",
+                        categories,
+                        source,
+                        Integer(page),
+                        date
+
+          begin
+            promise.validate!
+          rescue Hdo::StortingImporter::ValidationError => ex
+            errors << "row #{external_id}: #{ex.message}"
+            next
+          end
+
+          promise
+        }.compact
+
+        if errors.any?
+          raise "found errors:\n#{errors.join("\n")}"
         end
 
-        result
+        promises
       end
 
       def self.from_hash(hash)
